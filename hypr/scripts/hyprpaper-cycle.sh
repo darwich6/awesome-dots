a5d9d78c3f62d8bf335fa86eac7f8ab141a2cd81
Ahmed Darwich <adarwich2010@gmail.com>
2026-02-10 10:12:31 -0600

    feat: add more nixos config

---
+++ hypr/scripts/hyprpaper-cycle.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DIR="FILE LOCATION OF WALLPAPERS"
+INTERVAL=900
+MON1="DP-2"
+MON2="DP-1"
+
+while true; do
+  img="$(find "$DIR" -type f \( -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.png' -o -iname '*.webp' \) -print0 | shuf -z -n 1 | tr -d '\0')"
+
+  hyprctl hyprpaper preload "$img" >/dev/null
+  hyprctl hyprpaper wallpaper "$MON1,$img" >/dev/null
+  hyprctl hyprpaper wallpaper "$MON2,$img" >/dev/null
+
+  sleep "$INTERVAL"
+done
---
+++ nvim/lua/plugins/dracula.lua
@@ -0,0 +1,11 @@
+return {
+  "Mofiqul/dracula.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("dracula").setup({
+      transparent_bg = false,
+      italic_comment = true,
+    })
+  end,
+}
